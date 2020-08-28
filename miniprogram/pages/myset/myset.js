e784ccd72cbe31c3481ee6936ff59a9aaed2c9a0
dcy00 <18435104170@163.com>
2020-08-28 15:42:05 +0800

    常见APIs

---
+++ miniprogram/pages/myset/myset.js
@@ -0,0 +1,85 @@
+// pages/myset/myset.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     launchoptions:""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const lo = wx.getLaunchOptionsSync();  //与APP.onLaunch的回调参数一致
+    this.setData({
+      launchoptions: JSON.stringify(lo)
+    })
+    wx.login({
+      success (res) {
+        if (res.code) {
+          //发起网络请求
+          // wx.request({
+          //   url: 'https://test.com/onLogin',
+          //   data: {
+          //     code: res.code
+          //   }
+          // })
+          console.log(res.code)
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
